96c8c7a1f9a14f22d972d425d1d0978830aa71a8
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-08-01 21:24:45 -0500

    create helloName function

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,10 +15,11 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  let greeting = 'Hello, ' + name + '!';
+  return greeting;
 }
 // Remember to call the function to test
-
+console.log('Test - should say "Hello, Maggie!"', helloName('Maggie'));
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber , secondNumber) {
